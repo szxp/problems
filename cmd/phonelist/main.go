02b6c617a98012e84c02e6f580a616c29a9a8bde
Péter Szakszon <szp0530@gmail.com>
2021-12-12 16:31:28 +0100

    Add phonelist exercise

---
+++ cmd/phonelist/main.go
@@ -0,0 +1,119 @@
+// Phone List
+// Given a list of phone numbers, determine if it is consistent in the sense that no number is the prefix of another. Let’s say the phone catalogue listed these numbers:
+// 
+// Emergency 911
+// 
+// Alice 97 625 999
+// 
+// Bob 91 12 54 26
+// 
+// In this case, it’s not possible to call Bob, because the central would direct your call to the emergency line as soon as you had dialled the first three digits of Bob’s phone number. So this list would not be consistent.
+// 
+// Input
+// The first line of input gives a single integer, 1≤t≤40, the number of test cases. Each test case starts with n, the number of phone numbers, on a separate line, 1≤n≤10000. Then follows n lines with one unique phone number on each line. A phone number is a sequence of at most ten digits. Note that leading zeros in phone numbers are significant, e.g., “0911” is a different phone number than “911”.
+// 
+// Output
+// For each test case, output “YES” if the list is consistent, or “NO” otherwise.
+// 
+// Sample Input 1	
+// 2
+// 3
+// 911
+// 97625999
+// 91125426
+// 5
+// 113
+// 12340
+// 123440
+// 12345
+// 98346
+// 
+// Sample Output 1
+// NO
+// YES
+
+package main
+
+import (
+    "log"
+    "bufio"
+    "os"
+    "strconv"
+)
+
+func main() {
+    w := bufio.NewWriter(os.Stdout)
+    scanner := bufio.NewScanner(os.Stdin)
+
+    scanner.Scan()
+    s := scanner.Text()
+    t, err := strconv.Atoi(s)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    for i:=0; i<t; i++ {
+        scanner.Scan()
+        s := scanner.Text()
+        n, err := strconv.Atoi(s)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        tree := make([]*digit, 10)
+        found := false
+        for k:=0; k<n; k++ {
+            scanner.Scan()
+            s := scanner.Text()
+            found = found || hasPrefix(s, tree)
+        }
+
+        if found {
+            w.Write([]byte("NO\n"))
+        } else {
+            w.Write([]byte("YES\n"))
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatalln(err)
+    }
+
+    w.Flush()
+}
+
+
+type digit struct {
+    digit []*digit
+    end   bool
+}
+
+func hasPrefix(s string, tree []*digit) bool {
+    var d int
+    for i, c := range s {
+        d = int(c) - 48
+
+        if tree[d] != nil && tree[d].end {
+            return true
+        }
+
+        if i == len(s)-1 && tree[d] != nil {
+            return true
+        }
+
+        if tree[d] == nil {
+            tree[d] = &digit{
+                digit: make([]*digit, 10),
+            }
+        }
+
+        if i == len(s)-1 {
+           tree[d].end = true
+        }
+
+        tree = tree[d].digit
+    }
+    return false
+}
+
+
